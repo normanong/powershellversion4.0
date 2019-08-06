e39902d4543c77f9a227f7344204e635f041bec9
normanong <50363374+normanong@users.noreply.github.com>
2019-08-06 11:27:49 +0800

    Add files via upload
    sample of inserting data into database

---
+++ inserdata.ps1
@@ -0,0 +1,17 @@
+﻿clear-host
+$data=import-csv C:\Alert\completedata.csv
+$datacount=($data).count 
+for($a=0;$a -lt $datacount; $a++){
+$condition=$data.name[$a]
+$count=$data.numberofrun[$a]
+$lastruntime=$data.lastruntime[$a]
+
+$insertquery="
+INSERT INTO [dbo].[servicetable]
+([stating],[name],[displayname])
+VALUES
+('$condition','$count','$lastruntime')
+GO
+"
+Invoke-SQLcmd -ServerInstance "192.168.200.130" -query $insertquery -U sa -Password gmtonline -Database ProjMgmt
+}
