d310bb56d88d5b0baba1162d6b05139fe50efbc2
normanong <50363374+normanong@users.noreply.github.com>
2019-08-06 11:21:02 +0800

    Add files via upload
    to clean all the thing

---
+++ refresher.ps1
@@ -0,0 +1,4 @@
+﻿ Remove-Item –path c:\Alert\record1.csv –recurse 
+ Remove-Item –path c:\Alert\recordtime1.csv –recurse 
+  Remove-Item –path c:\Alert\record.csv –recurse 
+ Remove-Item –path c:\Alert\recordtime.csv –recurse 
